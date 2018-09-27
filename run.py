fe295c8da928dc5769c7e6fc823f777ff2b7bc6d
dan <hyounguk1112@gmail.com>
2018-09-27 20:27:11 +0900

    update

--- run.py
+++ run.py
@@ -16,16 +16,19 @@ if not os.path.isdir(result_path):
     os.makedirs(result_path)
 
 for file in os.listdir(bg_path):
+    bg_group = None
     with open(os.path.join(bg_path, file) ,'rb') as fd:
         bg_psd = pytoshop.read(fd)
         bg_nested = nested_layers.psd_to_nested_layers(bg_psd)
         for l in bg_nested:
             if l._name == 'bg':
                 bg_group = l
+
         with open(os.path.join(bc_path, file) , 'rb') as fd:
             bc_psd = pytoshop.read(fd)
             bc_nested = nested_layers.psd_to_nested_layers(bc_psd)
-            bc_nested.insert(len(bc_nested)-1, bg_group)
+            if bg_group != None:
+            	bc_nested.insert(len(bc_nested)-1, bg_group)
 
             with open(os.path.join(result_path, file), 'wb') as fd:
                 bc_psd = nested_layers.nested_layers_to_psd(bc_nested, bc_psd._color_mode)
